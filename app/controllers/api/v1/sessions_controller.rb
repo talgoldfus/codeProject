1c601428c25f7cc8ccd233e730a2e547fd019b22
abillig <avrambillig@gmail.com>
2016-08-13 18:00:19 -0400

    created sessions controller and controller actions

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,22 @@
+module Api
+  module V1
+    class SessionsController < ApplicationController
+
+      def create
+        if @user = User.find_by(first_name: params[:name])
+          if @user.authenticate(params[:password])
+              session[:user_id] = @user.id
+              current_user = @user
+              render json: current_user
+          end
+        end
+      end
+
+      def destroy
+        session[:user_id] = nil
+        redirect_to root_url
+      end
+
+    end
+  end
+end
