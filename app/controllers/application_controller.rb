5aeca6525c675338d4fe311253d6f9b8cd8ac6cb
abillig <avrambillig@gmail.com>
2016-08-13 17:59:54 -0400

    added authentication and password protection with digest

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::Serialization
+  # helper_method :current_user
+  # helper_method :logged_in?
+
+
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
