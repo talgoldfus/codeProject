0b9a95386c14fd346b27fdaf5302e8de19f837bd
Tal Goldfus <talgoldfus@gmail.com>
2016-08-15 20:59:49 -0400

    Solved flickering/ rerendiering issue by adding componentWillUpdate to currentQuestion

--- codeChallenge/src/components/CurrentQuestion.js
+++ codeChallenge/src/components/CurrentQuestion.js
@@ -9,15 +9,13 @@ class CurrentQuestion extends Component {
 
 
   componentWillUpdate(nextProps) {
-
     if (nextProps.params.questionId !== this.props.params.questionId){
-    this.props.getOptions(this.props.params.questionId)
+    this.props.getOptions(nextProps.params.questionId)
     return true}
     return false
   }
 
   render(){
-    debugger
     const optionsArray = this.props.info.options || []
     const optionsComponents = optionsArray.map(option=>{
       return <Option content={option.content} key={option.id} correct={option.correct}
