1910260624e455d399a421db1eef540d9c5e741a
Tal Goldfus <talgoldfus@gmail.com>
2016-08-15 20:44:30 -0400

    Trying to change component lifecycle

--- codeChallenge/src/components/CurrentQuestion.js
+++ codeChallenge/src/components/CurrentQuestion.js
@@ -7,12 +7,17 @@ class CurrentQuestion extends Component {
     this.props.getOptions(this.props.params.questionId)
   }
 
-  componentWillUpdate(){
-    debugger
+
+  componentWillUpdate(nextProps) {
+
+    if (nextProps.params.questionId !== this.props.params.questionId){
     this.props.getOptions(this.props.params.questionId)
+    return true}
+    return false
   }
 
   render(){
+    debugger
     const optionsArray = this.props.info.options || []
     const optionsComponents = optionsArray.map(option=>{
       return <Option content={option.content} key={option.id} correct={option.correct}
