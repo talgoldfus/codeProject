599cd899d79dae470283cd4fa472890b46239802
abillig <avrambillig@gmail.com>
2016-08-18 18:45:16 -0400

    begin board analytics

---
+++ Rails/app/services/board_analytics.rb
@@ -0,0 +1,22 @@
+# class BoardAnalytics 
+
+#   def self.run(board_id)
+#     board = Board.find(board_id)
+#     return {topic: board.name, categories: 
+#     average_score = board.average_score
+#     board_categories = board.categories
+#     board_categories.each do |category|
+#     category_ {category.name: }
+#       category.questions.each do |question|
+#         byebug 
+#       end
+#     end
+#   end 
+
+
+#   def find_average_score(board)
+#     return {"percentage_correct": board.average_score}
+#   end
+
+
+# end 
