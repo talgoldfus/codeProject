fdd2c3e265feec16cb92c7f9c87524c63bce4eff
abillig <avrambillig@gmail.com>
2016-08-18 16:50:20 -0400

    added board percentage analytics and game-board associations

--- Rails/app/models/game.rb
+++ Rails/app/models/game.rb
@@ -2,5 +2,6 @@ class Game < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :board, optional: true
   has_many :game_options
+  has_many :options, through: :game_options
 
 end
--- Rails/app/models/option.rb
+++ Rails/app/models/option.rb
@@ -1,4 +1,6 @@
 class Option < ApplicationRecord
   belongs_to :question, optional: true
   has_many :game_options
+  has_many :games, through: :game_options 
+
 end
