9eee6a084671a8ff509fa49bf49cc71c74bbc0c0
abillig <avrambillig@gmail.com>
2016-08-31 13:05:07 -0400

    style user page chart.js rows

--- Rails/app/services/question_hash_creator.rb
+++ Rails/app/services/question_hash_creator.rb
@@ -3,6 +3,7 @@ class QuestionHashCreator
 def self.run(collection)
     return collection.each_with_object({}) do |question, hash|
       question_hash = {}
+      question_hash["id"]= question.id
       question_hash["content"] = question.content
       question_hash["difficulty"] = question.difficulty
       question_hash["percentage_right"] = question.percentage_correct
